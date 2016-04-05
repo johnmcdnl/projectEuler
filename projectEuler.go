14b26d52d15d93b5f89e11922f90b82dfd7a4875
johnmcdnl <john.mcdonnell439@gmail.com>
2016-04-06 00:57:43 +0100

    clean up

--- projectEuler.go
+++ projectEuler.go
@@ -3,16 +3,9 @@ package main
 import (
 	"github.com/johnmcdnl/projectEuler/problems"
 	"fmt"
-	"github.com/johnmcdnl/projectEuler/utils"
 )
 
 func main() {
 	fmt.Print(problems.Problem007())
-
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-
-	fmt.Println(utils.IsPrime(uint64(18446744073709551557 )))
 
 }
