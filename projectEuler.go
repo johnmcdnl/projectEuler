66a12a2ffd632e65dea0cd1df32fcc587e07c5e3
johnmcdnl <john.mcdonnell439@gmail.com>
2016-04-21 21:27:13 +0100

    solving 2

--- projectEuler.go
+++ projectEuler.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main() {
-	fmt.Println(problems.Problem003())
+	fmt.Println(problems.Problem002())
 }
--- utils/mathUtils.go
+++ utils/mathUtils.go
@@ -64,3 +64,28 @@ func GetAllFactors(num int) []int {
 	return factors
 }
 
+
+func GenerateFibonacciUntil(max int) []int {
+
+	a := 1
+	b := 2
+	fibs := []int{1, 2}
+	for (b < max) {
+		a, b = b, a + b
+		fibs = append(fibs, b)
+	}
+	return fibs[:len(fibs) - 1]
+
+}
+
+func GenerateNFibonacciValues(num int) []int{
+	a := 1
+	b := 2
+	fibs := []int{1, 2}
+	for (len(fibs) < num) {
+		a, b = b, a + b
+		fibs = append(fibs, b)
+	}
+	return fibs
+}
+
