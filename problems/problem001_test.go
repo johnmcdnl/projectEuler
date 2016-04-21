a5f74e4b1a7eb9ff68714bb76826d1b4aa564520
johnmcdnl <john.mcdonnell439@gmail.com>
2016-04-21 20:59:33 +0100

    adding tests

---
+++ problems/problem001_test.go
@@ -0,0 +1,16 @@
+package problems
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func BenchmarkProblem001(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Problem001()
+	}
+}
+
+func TestProblem001(t *testing.T) {
+	assert.Equal(t, Problem001(), int(233168))
+}
---
+++ problems/problem007_test.go
@@ -0,0 +1,16 @@
+package problems
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func BenchmarkProblem007(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Problem007()
+	}
+}
+
+func TestProblem007(t *testing.T) {
+	assert.Equal(t, Problem007(), int(104743))
+}
