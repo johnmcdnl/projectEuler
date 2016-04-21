9201c7fd7a337cda00d92b2c1056603708ce9397
johnmcdnl <john.mcdonnell439@gmail.com>
2016-04-21 21:06:13 +0100

    solving problem 3

--- problems/problem003.go
+++ problems/problem003.go
@@ -1,7 +1,6 @@
 package problems
 
 import (
-	"fmt"
 	"github.com/johnmcdnl/projectEuler/utils"
 )
 
@@ -13,7 +12,17 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
  */
 func Problem003() int {
-	fmt.Println(mathUtils.GetAllFactors(100))
-	return 0
+
+	factors := mathUtils.GetAllFactors(600851475143)
+
+	largestPrimeFactor := 0
+
+	for _, factor := range factors {
+		if mathUtils.IsPrime(factor) && factor > largestPrimeFactor {
+			largestPrimeFactor = factor
+		}
+	}
+
+	return largestPrimeFactor
 }
 
