02eaef28680f61c28f41488ee292343461061c23
johnmcdnl <john.mcdonnell439@gmail.com>
2016-05-11 21:46:05 +0100

    add simple test for 50

---
+++ problems/50/problem050_test.go
@@ -0,0 +1,16 @@
+package problems
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func BenchmarkProblem050(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Problem050()
+	}
+}
+
+func TestProblem050(t *testing.T) {
+	assert.Equal(t, Problem050(), 41)
+}
