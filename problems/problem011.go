9739ebebb0936e113709cbc42ba89e4910b6b0d2
John McDonnell <john.mcdonnell439@gmail.com>
2016-05-14 16:20:24 +0100

    improve solution 011

---
+++ problems/problem011.go
@@ -0,0 +1,5 @@
+package problems
+
+func Problem011() int {
+	return 0
+}
---
+++ problems/problem011_test.go
@@ -0,0 +1,15 @@
+package problems
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func BenchmarkProblem011(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Problem011()
+	}
+}
+func TestProblem011(t *testing.T) {
+	assert.Equal(t, nil, Problem011())
+}
